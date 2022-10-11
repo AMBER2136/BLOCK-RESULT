ea5ab9c7168f736ff38f0864379a11eed66b1528
AMBER2136 <mikhail-grishkov@yandex.ru>
2022-10-11 21:16:35 +0300

    WARMIN UP

--- The Task/Program.cs
+++ The Task/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] array  = new string[]{"ДА","СБУДУТСЯ", "МЕЧТЫ", "БИЛЛИ", "БОНСА"};
+Console.WriteLine($"{array[2]}++++++++{array[0]}");
+
+string str1 = "BOBO-MOBO";
+string str2= "";
+int l = str1.Length;
+Console.WriteLine(l);
+int m = str2.Length;
+Console.WriteLine(m);
+int o = array[0].Length;
+Console.WriteLine(o);
